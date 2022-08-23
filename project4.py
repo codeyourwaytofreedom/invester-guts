2d53a3865385b19781b779ef1902565cb0745789
codeyourwaytofreedom <can.kolej@gmail.com>
2022-08-23 12:01:36 +0300

    new pages added form activated

--- project4.py
+++ project4.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 invester = Flask(__name__)
 invester.config["SECRET_KEY"] = "dtjdtjtkt4758"
 
-@invester.route("/")
+@invester.route("/", methods=['POST', 'GET'])
 def home():
+    if request.method == 'POST':
+        return render_template('wallets.html')
     return render_template("home.html")
 
 if __name__ == "__main__":
